125f1051ca748207b1d5eaac5d042e36a96496b2
yap cheah shen <yapcheahshen@gmail.com>
2023-05-19 10:39:54 +0800

    one more lecture

--- youtube.js
+++ youtube.js
@@ -5,4 +5,15 @@ export const YoutubeId={
 "004-戒律對修道的重要":"lLUvXwQE6b8",
 "005-戒律對修道的重要":"qHOUgeng6Nc",
 "006-戒律對修道的重要":"sFLybjwqutg",
+
+"001-戒法戒體要義淺說":"Tg-RVGlMKFk",
+"002-戒法戒體要義淺說":"LLI8Lh-QvCw",
+"003-戒法戒體要義淺說":"1ne17YoNFsI",
+"004-戒法戒體要義淺說":"AWg_7F1yFhA",
+"005-戒法戒體要義淺說":"Y9fAA6YqMZ4",
+"006-戒法戒體要義淺說":"jf_fq14o7R4",
+"007-戒法戒體要義淺說":"EcE04MHfDqc",
+"008-戒法戒體要義淺說":"zKClB5F4DX4",
+"009-戒法戒體要義淺說":"KxzBgtp571M",
+
 }
